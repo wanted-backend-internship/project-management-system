01cb234d0c1da5a9621f5658222f7e7835224208
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-29 12:01:47 +0900

    [BE] Setting: 프로젝트 생성

---
+++ backend/pmsystem/src/test/java/wanted/assignment/pmsystem/PmsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package wanted.assignment.pmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
