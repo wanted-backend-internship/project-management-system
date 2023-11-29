e4d7ccc9b508c450eb38613a6805098a22809fe2
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-29 12:24:32 +0900

    [FE] Setting: 프로젝트 생성

---
+++ frontend/src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createPinia } from 'pinia';
+import BootstrapVue3 from 'bootstrap-vue-3';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import Router from './router';
+
+const app = createApp(App);
+
+app
+    .use(createPinia())
+    .use(BootstrapVue3)
+    .use(Router)
+    .mount('#app')
